5714978dec3cb6841ab1fa93658155a2149acca3
lucianommatos <lucianommatos@gmail.com>
2020-08-22 16:15:16 +0100

    Initial project commit

---
+++ src/main/java/com/project/lmspringpetclinic/LmSpringPetclinicApplication.java
@@ -0,0 +1,13 @@
+package com.project.lmspringpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmSpringPetclinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmSpringPetclinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/lmspringpetclinic/LmSpringPetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.lmspringpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmSpringPetclinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
