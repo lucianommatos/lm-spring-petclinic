9a34c31792397ac3ba3c3bd00e7b9f19632fa63e
lucianommatos <lucianommatos@gmail.com>
2020-09-03 16:56:01 +0100

    Adding Map services. Closes 5

---
+++ pet-clinic-data/src/main/java/com/project/lmspringpetclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,8 @@
+package com.project.lmspringpetclinic.services.map;
+
+import com.project.lmspringpetclinic.model.Owner;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> {
+
+	
+}
---
+++ pet-clinic-data/src/main/java/com/project/lmspringpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,7 @@
+package com.project.lmspringpetclinic.services.map;
+
+import com.project.lmspringpetclinic.model.Pet;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> {
+
+}
