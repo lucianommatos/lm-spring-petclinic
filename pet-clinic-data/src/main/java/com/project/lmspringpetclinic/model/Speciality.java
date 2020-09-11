78620c96309352bba31509728d4489cac4acb7d5
lucianommatos <lucianommatos@gmail.com>
2020-09-11 16:53:25 +0100

    Created Speciality Entity and associated with Vet. closes #27

---
+++ pet-clinic-data/src/main/java/com/project/lmspringpetclinic/model/Speciality.java
@@ -0,0 +1,17 @@
+package com.project.lmspringpetclinic.model;
+
+public class Speciality extends BaseEntity {
+
+	private static final long serialVersionUID = -279794528964127959L;
+	
+	private String description;
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+}
--- pet-clinic-data/src/main/java/com/project/lmspringpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/project/lmspringpetclinic/model/Vet.java
@@ -1,10 +1,19 @@
 package com.project.lmspringpetclinic.model;
 
-public class Vet extends Person {
+import java.util.Set;
 
-	/**
-	 * 
-	 */
+public class Vet extends Person {
+	
 	private static final long serialVersionUID = -7513611102422546088L;
+	
+	private Set<Speciality> specialities;
+
+	public Set<Speciality> getSpecialities() {
+		return specialities;
+	}
+
+	public void setSpecialities(Set<Speciality> specialities) {
+		this.specialities = specialities;
+	}
 
 }
