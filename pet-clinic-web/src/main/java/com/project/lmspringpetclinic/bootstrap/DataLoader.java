d6c0f5b6ca2f17643bdee0380f2368b6730f7efa
lucianommatos <lucianommatos@gmail.com>
2020-09-11 17:38:57 +0100

    Added PetTypes to startup loaded data. closes #32

--- pet-clinic-web/src/main/java/com/project/lmspringpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/project/lmspringpetclinic/bootstrap/DataLoader.java
@@ -5,8 +5,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.project.lmspringpetclinic.model.Owner;
+import com.project.lmspringpetclinic.model.PetType;
 import com.project.lmspringpetclinic.model.Vet;
 import com.project.lmspringpetclinic.services.OwnerService;
+import com.project.lmspringpetclinic.services.PetTypeService;
 import com.project.lmspringpetclinic.services.VetService;
 
 @Component
@@ -14,15 +16,26 @@ public class DataLoader implements CommandLineRunner {
 	
 	private final OwnerService ownerService;
 	private final VetService vetService;
+	private final PetTypeService petTypeService;
 	
 	@Autowired
-	public DataLoader(OwnerService ownerService, VetService vetService) {
+	public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
 		this.ownerService = ownerService;
 		this.vetService = vetService;
+		this.petTypeService = petTypeService;
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		PetType dog = new PetType();
+		dog.setName("Doggito");
+		PetType savedDogPetType = petTypeService.save(dog);
+		
+		PetType cat = new PetType();
+		cat.setName("Kittie");
+		PetType savedCatPetType = petTypeService.save(cat);
+		
 		
 		Owner owner1 = new Owner();
 		owner1.setFirstName("Luciano");
