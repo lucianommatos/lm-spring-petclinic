4bdbd28ce75dc1fc80761ff779fb1c2fb0b882eb
lucianommatos <lucianommatos@gmail.com>
2020-09-16 16:13:54 +0100

    Added Visit JPA entity. closes #38

--- pet-clinic-data/src/main/java/com/project/lmspringpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/com/project/lmspringpetclinic/model/Visit.java
@@ -2,12 +2,24 @@ package com.project.lmspringpetclinic.model;
 
 import java.time.LocalDate;
 
-public class Visit extends BaseEntity {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
-	private static final long serialVersionUID = -1645297808159831492L;
+@Entity
+@Table(name = "visits")
+public class Visit extends BaseEntity {
 	
+	@Column(name = "date")
 	private LocalDate date;
+	
+	@Column(name = "description")
 	private String description;
+	
+	@ManyToOne
+	@JoinColumn(name = "pet_id")
 	private Pet pet;
 	
 	public LocalDate getDate() {
