c3935a2ca2aab356b37a52f73fb9dbd2627163d7
lucianommatos <lucianommatos@gmail.com>
2020-09-18 12:40:21 +0100

    Created Visit Map Service implementation. closes #46

---
+++ pet-clinic-data/src/main/java/com/project/lmspringpetclinic/services/VisitService.java
@@ -0,0 +1,7 @@
+package com.project.lmspringpetclinic.services;
+
+import com.project.lmspringpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/com/project/lmspringpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,41 @@
+package com.project.lmspringpetclinic.services.map;
+
+import java.util.Set;
+
+import com.project.lmspringpetclinic.model.Visit;
+import com.project.lmspringpetclinic.services.VisitService;
+
+public class VisitMapService extends AbstractMapService<Visit> implements VisitService {
+	
+	@Override
+	public Set<Visit> findAll() {
+		return super.findAll();
+	}
+	
+	@Override
+	public Visit findById(Long id) {
+		return super.findById(id);
+	}
+	
+	@Override
+	public Visit save(Visit object) {
+		
+		if (object.getPet() == null || object.getPet().getOwner() == null || object.getPet().getId() == null
+				|| object.getPet().getOwner().getId() == null) {
+			throw new RuntimeException("Invalid Visit");
+		}
+		
+		return super.save(object);		
+	}
+	
+	@Override
+	public void delete(Visit object) {
+		super.delete(object);
+	}
+	
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+	}
+
+}
